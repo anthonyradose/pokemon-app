63ba3cf86e593325fed1f116d26f5539c301a4ac
Anthony Radose <asradose@gmail.com>
2024-11-18 16:42:55 +0000

    Begin refactoring of folders, files and components

--- src/app/App.js
+++ src/app/App.js
@@ -1,7 +1,7 @@
 import React from "react";
-import Pokedex from "./pages/Pokedex/Pokedex";
-import Pokemon from "./pages/Pokemon/Pokemon";
-import Nav from "./components/Nav/Nav";
+import Pokedex from "../pages/Pokedex/Pokedex";
+import Pokemon from "../pages/Pokemon/Pokemon";
+import Nav from "../components/Nav/Nav";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import "./App.css";
 
@@ -9,13 +9,18 @@ const App = () => {
   return (
     <div className="appContentContainer">
       <Router basename="/pokemon-app/">
+      <header>
         <Nav />
+        </header>
+        <main>
         <div className="appRoutesContainer">
           <Routes>
             <Route path="/" element={<Pokedex />} />
             <Route path="/:name" element={<Pokemon />} />
           </Routes>
         </div>
+        </main>
+        {/* <footer></footer> */}
       </Router>
     </div>
   );
