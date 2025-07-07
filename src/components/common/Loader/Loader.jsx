4003446c640251e46a80a3b829c0d7da1a480f45
Anthony Radose <asradose@gmail.com>
2025-07-07 17:25:17 +0100

    refactor: improve Loader component structure

--- src/components/common/Loader/Loader.jsx
+++ src/components/common/Loader/Loader.jsx
@@ -2,7 +2,13 @@ import React from "react";
 import styles from "./Loader.module.css";
 
 const Loader = () => {
-  return <div className={styles.pokeballLoader} />;
+  return (
+    <div 
+      className={styles.pokeballLoader}
+      role="status"
+      aria-label="Loading Pokémon data"
+    />
+  );
 };
 
 export default Loader;
